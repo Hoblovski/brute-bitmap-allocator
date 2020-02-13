3d88d45e135899c2cf49c9a74e3b1b1214eb1428
Hoblovski <daizy19@mails.tsinghua.edu.cn>
2020-02-13 20:14:53 +0800

    Support aligned allocations

--- src/lib.rs
+++ src/lib.rs
@@ -57,6 +57,32 @@ impl LinearBitMap {
         None
     }
 
+    /// Parameters:
+    /// * align: must be a power of two.
+    ///
+    /// Similar to alloc, but the returned index is aligned to align.
+    pub fn alloc_aligned(&self, n: usize, align: usize) -> Option<usize> {
+        assert!(0 < n && n <= self.size);
+        assert!(align >= 1 && align & (align-1) == 0); // alignment must be a power of 2
+        let align_mask = align - 1;
+        let mut bm = self.bitmap.lock();
+        let mut begin = 0;
+        while begin < self.size {
+            while begin < self.size && bm[begin] { begin += 1; }
+            begin = ((begin-1) & !align_mask) + align; // x >= begin s.t. x is aligned
+            if begin >= self.size { break; }
+            if bm[begin] { continue; } // advance to allocated position
+            let mut end = begin + 1;
+            while end < self.size && !bm[end] { end += 1; }
+            if end - begin >= n {
+                for i in begin..begin+n { bm[i] = true; }
+                return Some(begin);
+            }
+            begin = end;
+        }
+        None
+    }
+
     pub fn dealloc(&self, begin: usize, n: usize) {
         let mut bm = self.bitmap.lock();
         for i in begin..begin+n {
@@ -97,5 +123,23 @@ mod tests {
         assert_eq!(Some(6), bm.alloc(1));
         assert_eq!(None, bm.alloc(3));
         assert_eq!(Some(7), bm.alloc(2));
+    }
+
+    #[test]
+    fn test_alloc_aligned() {
+        let bm = LinearBitMap::new(10);
+
+        assert_eq!(Some(0), bm.alloc(5));
+        assert_eq!(Some(6), bm.alloc_aligned(3, 2));
+        assert_eq!(None, bm.alloc(2));
+        assert_eq!(Some(5), bm.alloc(1));
+        assert_eq!(Some(9), bm.alloc(1));
+        assert_eq!(None, bm.alloc(1));
+        bm.dealloc(4, 6);
+        assert_eq!(Some(8), bm.alloc_aligned(2, 8));
+        assert_eq!(Some(4), bm.alloc(1));
+        assert_eq!(None, bm.alloc_aligned(2, 4));
+        assert_eq!(Some(6), bm.alloc_aligned(2, 2));
+        assert_eq!(Some(5), bm.alloc(1));
     }
 }
